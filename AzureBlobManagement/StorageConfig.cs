aa46ef7d73a3c3e301dec35bfb7787590a76259a
Abdullah Aziz Sharfuddin Shakkhor <abdullahshakkhor@gmail.com>
2021-12-05 10:47:42 +0600

    Add project files.

---
+++ AzureBlobManagement/StorageConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureBlobManagement
+{
+    public class StorageConfig
+    {
+        public string StorageAccountName_Source { get; set; }
+        public string StorageAccessKey_Source { get; set; }
+        public string EcapRootContainerName_Source { get; set; }
+        public string TenantRootDirectoryName_Source { get; set; }
+        public string StorageAccountName_Destination { get; set; }
+        public string StorageAccessKey_Destination { get; set; }
+        public string EcapRootContainerName_Destination { get; set; }
+        public string TenantRootDirectoryName_Destination { get; set; }
+        public string UserId { get; set; }
+        public string TenantDatabase { get; set; }
+        public string DbConnectionString { get; set; }
+    }
+}
